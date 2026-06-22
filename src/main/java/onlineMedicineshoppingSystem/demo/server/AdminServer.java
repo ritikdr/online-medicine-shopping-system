09f4aa0f85f3f1d1c5d98f60331dc13b8d78a76e
Ritik Kumar <ritik31dr@gmail.com>
2026-06-22 22:54:56 +0530

    update adminServer file

--- src/main/java/onlineMedicineshoppingSystem/demo/server/AdminServer.java
+++ src/main/java/onlineMedicineshoppingSystem/demo/server/AdminServer.java
@@ -3,6 +3,7 @@ package onlineMedicineshoppingSystem.demo.server;
 import onlineMedicineshoppingSystem.demo.entity.Admin;
 import onlineMedicineshoppingSystem.demo.repository.AdminRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,6 +15,9 @@ public class AdminServer {
     @Autowired
     private AdminRepository adminRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public List<Admin> getAllAdmin() {
         return adminRepository.findAll();
     }
@@ -23,6 +27,9 @@ public class AdminServer {
     }
 
     public Admin saveAdmin(Admin admin) {
+        if(admin.getPassword() != null && admin.getPassword().startsWith("$2a$")) {
+            admin.setPassword(passwordEncoder.encode(admin.getPassword()));
+        }
         return adminRepository.save(admin);
     }
 
